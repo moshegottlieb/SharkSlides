736719ae80db4d3ab54206cc0aa574ef8a961772
Moshe Gottlieb <moshe@sharkfood.com>
2015-11-25 17:04:56 +0200

    Initial Commit

---
+++ Shark Slides/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shark Slides
+//
+//  Created by Moshe Gottlieb on 25/11/2015.
+//  Copyright © 2015 Sharkfood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
